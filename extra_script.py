8efb7ebd8210619a043bc67a2eecd2f9d0a3bfb4
Maksims Kuznecovs <maksims.kuznecovs@flandersmake.be>
2023-04-06 17:17:21 +0200

    Cleanup

--- extra_script.py
+++
@@ -1,9 +0,0 @@
-Import('env')
-from os.path import join, realpath
-
-global_env = DefaultEnvironment()
-global_env.Append(
-    CPPDEFINES=[
-        ("UA_ARCHITECTURE_FREERTOSLWIP", 1), ("UA_ENABLE_AMALGAMATION", 1), ("UA_ARCHITECTURE_FREERTOSLWIP_POSIX_CLOCK", 1)
-    ]
-)
