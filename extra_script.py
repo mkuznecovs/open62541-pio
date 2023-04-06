4e494f5b23a65c04f51a2f59520c03ada02d52fb
Maksims Kuznecovs <maksims.kuznecovs@flandersmake.be>
2023-04-06 15:45:16 +0200

    Try to add extraScript

--- extra_script.py
+++ extra_script.py
@@ -1,6 +1,9 @@
 Import('env')
 from os.path import join, realpath
 
-# Pass flags to the Global environemnt (project `src` files, frameworks)
 global_env = DefaultEnvironment()
-global_env.Append(CPPDEFINES=[("UA_ARCHITECTURE_FREERTOSLWIP", 1)])
+global_env.Append(
+    CPPDEFINES=[
+        ("UA_ENABLE_AMALGAMATION", 1),
+    ]
+)
